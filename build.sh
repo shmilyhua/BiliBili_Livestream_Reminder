ac06d483d139fb801794cc1a70b5c8d0f8e066c6
Kevin Sun <55240339+shmilyhua@users.noreply.github.com>
2026-04-23 22:52:14 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t bilibili-livestream-reminder-complier .
-
-docker run --rm \
-    -e USER_ID="$UID" \
-    -e GROUP_ID="$GID" \
-    --mount type=bind,src="$PWD",dst=/code \
-    bilibili-livestream-reminder-complier
